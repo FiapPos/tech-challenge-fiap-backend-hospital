f61c7cc71e1980159c71b3f62415749adba55462
Sonia A. Ribeiro <sonia.ribeiro@alumni.usp.br>
2025-09-28 18:37:25 -0300

    chore: unifica dados e evento de agendamento

---
+++ appointment-service/src/main/java/com/fiap/techchallenge/appointment_service/core/enums/ESagaStatus.java
@@ -0,0 +1,5 @@
+package com.fiap.techchallenge.appointment_service.core.enums;
+
+public enum ESagaStatus {
+    SUCCESS, FAIL
+}
---
+++ notificacao-service/src/main/java/com/fiap/techchallenge/notificacao_service/core/dto/DadosAgendamento.java
@@ -0,0 +1,59 @@
+package com.fiap.techchallenge.notificacao_service.core.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class DadosAgendamento implements Serializable {
+
+    private long usuarioId;
+    private long hospitalId;
+    private long medicoId;
+    private long agendamentoId;
+
+    private String nomePaciente;
+    private String nomeMedico;
+    private String nomeHospital;
+    private String enderecoHospital;
+    private String especializacao;
+    private LocalDateTime dataHoraAgendamento;
+    private LocalDateTime criadoEm;
+    private LocalDateTime atualizadoEm;
+
+    public DadosAgendamento(long usuarioId,
+                            long hospitalId,
+                            long medicoId,
+                            String nomePaciente,
+                            String nomeMedico,
+                            String nomeHospital,
+                            String enderecoHospital,
+                            String especializacao,
+                            LocalDateTime dataHoraAgendamento,
+                            LocalDateTime criadoEm) {
+        this.usuarioId = usuarioId;
+        this.hospitalId = hospitalId;
+        this.medicoId = medicoId;
+        this.nomePaciente = nomePaciente;
+        this.nomeMedico = nomeMedico;
+        this.nomeHospital = nomeHospital;
+        this.enderecoHospital = enderecoHospital;
+        this.especializacao = especializacao;
+        this.dataHoraAgendamento = dataHoraAgendamento;
+        this.criadoEm = criadoEm;
+    }
+
+    public String getDataHoraFormatada() {
+        return getDataHoraAgendamento().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
+    }
+}
