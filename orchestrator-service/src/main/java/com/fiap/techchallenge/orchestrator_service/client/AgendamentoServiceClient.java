a5acebfbc85f58a84355d9ae609f3fddfc9896a9
Sonia A. Ribeiro <sonia.ribeiro@alumni.usp.br>
2025-10-04 10:17:31 -0300

    refat: remove comentario desnecessario

--- orchestrator-service/src/main/java/com/fiap/techchallenge/orchestrator_service/client/AgendamentoServiceClient.java
+++ orchestrator-service/src/main/java/com/fiap/techchallenge/orchestrator_service/client/AgendamentoServiceClient.java
@@ -8,35 +8,28 @@ import java.util.List;
 @FeignClient(name = "agendamento-service", url = "${services.agendamento.url}")
 public interface AgendamentoServiceClient {
 
-    // --- CRIAÇÃO ---
     @PostMapping("/api/consultas")
     DadosAgendamento criarConsultaPendente(@RequestBody DadosAgendamento consulta);
 
-    // --- COMPENSAÇÃO ---
     @DeleteMapping("/api/consultas/cancelar")
     void cancelarConsulta(@RequestBody DadosAgendamento consulta);
 
-    // --- CONFIRMAÇÃO ---
     @PutMapping("/api/consultas/confirmar")
     void confirmarConsulta(@RequestBody DadosAgendamento consulta);
 
-    // --- BUSCAR UM AGENDAMENTO PELO ID ---
     @GetMapping("/api/consultas/{id}")
     DadosAgendamento buscarConsulta(@PathVariable("id") Long id);
 
-    // --- LISTAR AGENDAMENTOS COM FILTROS ---
     @GetMapping("/api/consultas")
     List<DadosAgendamento> listarConsultas(
             @RequestParam(value = "patientId", required = false) String patientId,
             @RequestParam(value = "status", required = false) String status
     );
 
-    // Editar consulta
     @PutMapping("/api/consultas/{id}")
     void editarConsulta(@PathVariable("id") Long id,
                         @RequestBody DadosAgendamento consulta);
 
-    // --- REGISTRAR EVENTO DA SAGA ---
     @PostMapping("/api/consultas/{id}/eventos")
     void registrarEvento(
             @PathVariable("id") Long agendamentoId,
@@ -44,7 +37,6 @@ public interface AgendamentoServiceClient {
             @RequestBody SagaEventoDto evento
     );
 
-    // Buscar histórico de eventos da saga
     @GetMapping("/api/consultas/{id}/events")
     List<SagaEventoDto> buscarEventos(@PathVariable("id") Long id);
 }
